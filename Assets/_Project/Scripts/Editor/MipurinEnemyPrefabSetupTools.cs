f71b4f2dda067236795b94d20ff6ef1778431838
Ggoro Yattemiyo <goro.money.business@gmail.com>
2026-06-25 11:13:28 +0900

    Add HoneySlime prefab setup tool stub

---
+++ Assets/_Project/Scripts/Editor/MipurinEnemyPrefabSetupTools.cs
@@ -0,0 +1,11 @@
+using UnityEditor;
+using UnityEngine;
+
+public static class MipurinEnemyPrefabSetupTools
+{
+    [MenuItem("Mipurin/Setup/Formalize HoneySlime Enemy")]
+    public static void FormalizeHoneySlimeEnemy()
+    {
+        Debug.Log("HoneySlime formalize tool placeholder.");
+    }
+}
