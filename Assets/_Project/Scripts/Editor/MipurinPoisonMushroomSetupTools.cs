cf6b71d725af658347e57a48260c663583fd9a58
Ggoro Yattemiyo <goro.money.business@gmail.com>
2026-06-25 21:52:49 +0900

    Add PoisonMushroom setup tool stub

---
+++ Assets/_Project/Scripts/Editor/MipurinPoisonMushroomSetupTools.cs
@@ -0,0 +1,11 @@
+using UnityEditor;
+using UnityEngine;
+
+public static class MipurinPoisonMushroomSetupTools
+{
+    [MenuItem("Mipurin/Setup/Setup PoisonMushroom Enemy")]
+    public static void SetupPoisonMushroomEnemy()
+    {
+        Debug.Log("PoisonMushroom setup tool placeholder.");
+    }
+}
