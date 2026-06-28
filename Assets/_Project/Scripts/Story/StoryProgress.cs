44d51ef198127ac1df6e6e6edd67a072303c943f
Ggoro Yattemiyo <goro.money.business@gmail.com>
2026-06-28 16:14:21 +0900

    Add minimal story progress state

---
+++ Assets/_Project/Scripts/Story/StoryProgress.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public enum StoryStage
+{
+    NotStarted = 0,
+    QuestStarted = 1,
+    FirstAdventureReturned = 2
+}
+
+public class StoryProgress : MonoBehaviour
+{
+    private static StoryProgress instance;
+
+    [SerializeField] private StoryStage currentStage = StoryStage.NotStarted;
+
+    public static StoryProgress Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<StoryProgress>();
+            }
+
+            if (instance == null)
+            {
+                GameObject storyObject = new GameObject("StoryProgress");
+                instance = storyObject.AddComponent<StoryProgress>();
+            }
+
+            return instance;
+        }
+    }
+
+    public StoryStage CurrentStage => currentStage;
+    public bool IsQuestStarted => currentStage >= StoryStage.QuestStarted;
+    public bool HasReturnedFromFirstAdventure => currentStage >= StoryStage.FirstAdventureReturned;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void StartFirstForestQuest()
+    {
+        AdvanceTo(StoryStage.QuestStarted);
+    }
+
+    public void MarkFirstAdventureReturned()
+    {
+        AdvanceTo(StoryStage.FirstAdventureReturned);
+    }
+
+    public void ResetStoryForTest()
+    {
+        currentStage = StoryStage.NotStarted;
+    }
+
+    private void AdvanceTo(StoryStage nextStage)
+    {
+        if (nextStage <= currentStage)
+        {
+            return;
+        }
+
+        currentStage = nextStage;
+        Debug.Log("StoryProgress advanced to: " + currentStage);
+    }
+}
